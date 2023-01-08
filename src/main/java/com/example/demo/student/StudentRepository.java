be1f61d7f2b550ab7d4684edf3eb2494257fbc5e
Najifa Anjum <41308140+NajifaAnjum@users.noreply.github.com>
2023-01-08 22:24:32 +0600

    Update StudentRepository.java

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    //find student bt email
+    //find student by email
     @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
